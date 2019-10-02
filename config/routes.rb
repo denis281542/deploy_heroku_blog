5e8a1bd2472d7254018e96af29b874a9b871a486
denis281542 <deniskuimov@mail.ru>
2019-10-02 15:47:27 +0500

    Edit routes path new user

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
   
 
-  resources :blogs do 
-    resources :users
-  end
+  resources :blogs  
+  resources :users
+  
 
   root 'blogs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
