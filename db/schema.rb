b73b16854b95d3312c15e4be061aa7c87afe2b7c
denis281542 <deniskuimov@mail.ru>
2019-10-06 20:47:30 +0500

    Add column in table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_043803) do
+ActiveRecord::Schema.define(version: 2019_10_06_154244) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_10_04_043803) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
